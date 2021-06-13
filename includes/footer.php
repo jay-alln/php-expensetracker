c8750d1d64cb01a4a506a825a61d0204cddaf7fd
Jay Alln <benedictjay254@gmail.com>
2021-06-13 10:34:19 +0300

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<div class="col-sm-12">
+				<p class="back-link">Expense Tracker <span id="year"></span> </p>
+			</div>
+			<script>
+                let year = new Date();
+                document.querySelector("#year").innerHTML = year.getFullYear();
+
+            </script>			

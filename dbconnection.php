97f04860b0b51583537d61888de0565f4a6c5670
Joshua Benedict <76897373+jay-alln@users.noreply.github.com>
2021-10-19 16:15:28 +0300

    Add files via upload
    changed remote connection

---
+++ dbconnection.php
@@ -0,0 +1,18 @@
+<?php
+// production
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $db = "detsdb";
+
+// remote database connection
+$servername = "remotemysql.com";
+$username = "XxBaT2mkcz";
+$password = "ifJoXvdbDI";
+$db = "XxBaT2mkcz";
+
+$con=mysqli_connect($servername, $username, $password, $db);
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+?>
